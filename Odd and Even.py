011bea02ace378efb30e5595190328f39ada7564
Rehman575 <58176334+Rehman575@users.noreply.github.com>
2020-12-13 00:50:21 +0500

    Add files via upload

---
+++ Odd and Even.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+# Ask the user for a number. Depending on whether the number is even or odd, 
+#print out an appropriate message to the user.
+
+
+num = int(input("Enter any Number: "))
+if ( num % 2 == 0 ):
+    print("\nYou enter "+ str(num)+ " Which is Even ")
+else:
+    print("\nYou enter "+ str(num)+ " Which is Odd ")
+    
+
+
+# In[ ]:
+
+
+
+
